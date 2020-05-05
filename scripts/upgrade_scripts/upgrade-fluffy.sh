c00ad91a3a0106338648a732e0f124af3b60d0d1
TheGunter24 <thegunter24@gmail.com>
2020-05-05 13:04:44 +0200

    Update upgrade-fluffy.sh

--- scripts/upgrade_scripts/upgrade-fluffy.sh
+++ scripts/upgrade_scripts/upgrade-fluffy.sh
@@ -4,17 +4,26 @@
 #   upgrade script from flufflys timer   #
 ##########################################
 
-# change this setting to your install directory
-SERVERDIR="/srv/csgo-server/csgo"
+# change this to your csgo directory, if not running from "upgrade_scripts" folder
+SERVERDIR="../.."
 
 rm -rf "${SERVERDIR}/sound/quake"
-rm "${SERVERDIR}/sound/surftimer/*.mp3"
+rm -f "${SERVERDIR}/sound/surftimer/*.mp3"
 mv "${SERVERDIR}/sound/surftimer/pr/valve_logo_music.mp3" "${SERVERDIR}/sound/surftimer/pr.mp3"
 mv "${SERVERDIR}/sound/surftimer/top10/valve_logo_music.mp3" "${SERVERDIR}/sound/surftimer/top10.mp3"
-mv "${SERVERDIR}/sound/surftimer/wr/valve_logo_music.mp3" "${SERVERDIR}/sound/surftimer/wr.mp3"
+mv "${SERVERDIR}/sound/surftimer/wr/1/valve_logo_music.mp3" "${SERVERDIR}/sound/surftimer/wr.mp3"
 
-mv "${SERVERDIR}/cfg/sourcemod/surftimer/main.cfg" "${SERVERDIR}/cfg/sourcemod/surftimer/main.cfg.backup"
-mv "${SERVERDIR}/cfg/sourcemod/surftimer/map_types/surf_.cfg" "${SERVERDIR}/cfg/sourcemod/surftimer/surf.cfg.backup"
-rm -rf "${SERVERDIR}/cfg/sourcemod/surftimer/map_types/"
-rm "${SERVERDIR}/cfg/sourcemod/surftimer/*_.cfg"
-wget https://raw.githubusercontent.com/olokos/Surftimer-olokos/master/cfg/sourcemod/surftimer/main.cfg -P "${SERVERDIR}/cfg/sourcemod/surftimer/"
+mv "${SERVERDIR}/cfg/sourcemod/surftimer" "${SERVERDIR}/cfg/sourcemod/surftimer-old"
+
+rm -rf "${SERVERDIR}/optional"
+
+rm -f "${SERVERDIR}/addons/sourcemod/scripting/surftimer.smx"
+rm -f "${SERVERDIR}/addons/sourcemod/scripting/ckSurf-telefinder.sp"
+rm -f "${SERVERDIR}/addons/sourcemod/scripting/surftimer.smx"
+rm -rf "${SERVERDIR}/addons/sourcemod/scripting/surftimer"
+rm -f "${SERVERDIR}/addons/sourcemod/scripting/include/SurfTimer.inc"
+
+rm -f "${SERVERDIR}/addons/sourcemod/plugins/surftimer.smx"
+rm -f "${SERVERDIR}/addons/sourcemod/plugins/discord_api.smx"
+
+mv "${SERVERDIR}/addons/sourcemod/configs/surftimer" "${SERVERDIR}/addons/sourcemod/configs/surftimer-old"
