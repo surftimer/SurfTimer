08d4b02e04faed5ec4b8f106a04fd587c056dfbe
TheGunter24 <thegunter24@gmail.com>
2020-05-07 14:41:42 +0200

    Update upgrade-nikooo.sh

--- scripts/upgrade_scripts/upgrade-nikooo.sh
+++ scripts/upgrade_scripts/upgrade-nikooo.sh
@@ -4,22 +4,22 @@
 #   upgrade script from nikooo777 timer  #
 ##########################################
 
-# change this to your csgo directory, if not running from "upgrade_scripts" folder
-SERVERDIR="../.."
+# change below to your csgo server root directory
+SERVERDIR="/home/csgoserver/csgo/"
 
-rm -rf "${SERVERDIR}/sound/quake"
+rm -r "${SERVERDIR}/sound/quake"
 
 mv "${SERVERDIR}/cfg/sourcemod/ckSurf" "${SERVERDIR}/cfg/sourcemod/ckSurf-old"
 
-rm -f "${SERVERDIR}/addons/sourcemod/translations/ckSurf.phrases.txt"
+rm "${SERVERDIR}/addons/sourcemod/translations/ckSurf.phrases.txt"
 
-rm -f "${SERVERDIR}/addons/sourcemod/scripting/include/ckSurf.inc"
-rm -f "${SERVERDIR}/addons/sourcemod/scripting/ckSurf-telefinder.sp"
-rm -f "${SERVERDIR}/addons/sourcemod/scripting/ckSurf.sp"
-rm -rf "${SERVERDIR}/addons/sourcemod/scripting/ckSurf"
+rm "${SERVERDIR}/addons/sourcemod/scripting/include/ckSurf.inc"
+rm "${SERVERDIR}/addons/sourcemod/scripting/ckSurf-telefinder.sp"
+rm "${SERVERDIR}/addons/sourcemod/scripting/ckSurf.sp"
+rm -r "${SERVERDIR}/addons/sourcemod/scripting/ckSurf"
 
-rm -f "${SERVERDIR}/addons/sourcemod/plugins/ckSurf.smx"
-rm -f "${SERVERDIR}/addons/sourcemod/plugins/disabled/ckSurf-telefinder.smx"
-rm -f "${SERVERDIR}/addons/sourcemod/plugins/disabled/ckSurf-telefinder.smx"
+rm "${SERVERDIR}/addons/sourcemod/plugins/ckSurf.smx"
+rm "${SERVERDIR}/addons/sourcemod/plugins/disabled/ckSurf-telefinder.smx"
+rm "${SERVERDIR}/addons/sourcemod/plugins/disabled/ckSurf-telefinder.smx"
 
 mv "${SERVERDIR}/cfg/sourcemod/configs/ckSurf" "${SERVERDIR}/cfg/sourcemod/configs/ckSurf-old"
