29827d612c259aa7391ea1d6039e01ea8034a4d6
TheGunter24 <thegunter24@gmail.com>
2020-05-07 14:41:08 +0200

    Update upgrade-fluffy.sh

--- scripts/upgrade_scripts/upgrade-fluffy.sh
+++ scripts/upgrade_scripts/upgrade-fluffy.sh
@@ -4,26 +4,26 @@
 #   upgrade script from flufflys timer   #
 ##########################################
 
-# change this to your csgo directory, if not running from "upgrade_scripts" folder
-SERVERDIR="../.."
+# change below to your csgo server root directory
+SERVERDIR="/home/csgoserver/csgo/"
 
-rm -rf "${SERVERDIR}/sound/quake"
-rm -f "${SERVERDIR}/sound/surftimer/*.mp3"
+rm -r "${SERVERDIR}/sound/quake"
+rm "${SERVERDIR}/sound/surftimer/*.mp3"
 mv "${SERVERDIR}/sound/surftimer/pr/valve_logo_music.mp3" "${SERVERDIR}/sound/surftimer/pr.mp3"
 mv "${SERVERDIR}/sound/surftimer/top10/valve_logo_music.mp3" "${SERVERDIR}/sound/surftimer/top10.mp3"
 mv "${SERVERDIR}/sound/surftimer/wr/1/valve_logo_music.mp3" "${SERVERDIR}/sound/surftimer/wr.mp3"
 
 mv "${SERVERDIR}/cfg/sourcemod/surftimer" "${SERVERDIR}/cfg/sourcemod/surftimer-old"
 
-rm -rf "${SERVERDIR}/optional"
+rm -r "${SERVERDIR}/optional"
 
-rm -f "${SERVERDIR}/addons/sourcemod/scripting/surftimer.smx"
-rm -f "${SERVERDIR}/addons/sourcemod/scripting/ckSurf-telefinder.sp"
-rm -f "${SERVERDIR}/addons/sourcemod/scripting/surftimer.smx"
-rm -rf "${SERVERDIR}/addons/sourcemod/scripting/surftimer"
-rm -f "${SERVERDIR}/addons/sourcemod/scripting/include/SurfTimer.inc"
+rm "${SERVERDIR}/addons/sourcemod/scripting/surftimer.smx"
+rm "${SERVERDIR}/addons/sourcemod/scripting/ckSurf-telefinder.sp"
+rm "${SERVERDIR}/addons/sourcemod/scripting/surftimer.smx"
+rm -r "${SERVERDIR}/addons/sourcemod/scripting/surftimer"
+rm "${SERVERDIR}/addons/sourcemod/scripting/include/SurfTimer.inc"
 
-rm -f "${SERVERDIR}/addons/sourcemod/plugins/surftimer.smx"
-rm -f "${SERVERDIR}/addons/sourcemod/plugins/discord_api.smx"
+rm "${SERVERDIR}/addons/sourcemod/plugins/surftimer.smx"
+rm "${SERVERDIR}/addons/sourcemod/plugins/discord_api.smx"
 
 mv "${SERVERDIR}/addons/sourcemod/configs/surftimer" "${SERVERDIR}/addons/sourcemod/configs/surftimer-old"
