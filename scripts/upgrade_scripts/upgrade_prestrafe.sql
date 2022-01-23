573cc72d6825b645fd177f9328c639e7b7f66552
topvennie <vincent@vallaeys.com>
2022-01-23 19:02:52 +0100

    moved db upgrade script to new file

--- scripts/upgrade_scripts/upgrade_prestrafe.sql
+++ scripts/upgrade_scripts/upgrade_prestrafe.sql
@@ -9,5 +9,3 @@ ALTER TABLE ck_playertimes ADD velStartZ smallint(6) DEFAULT 0 NOT NULL;
 ALTER TABLE ck_wrcps ADD velStartXY smallint(6) DEFAULT 0 NOT NULL;
 ALTER TABLE ck_wrcps ADD velStartXYZ smallint(6) DEFAULT 0 NOT NULL;
 ALTER TABLE ck_wrcps ADD velStartZ smallint(6) DEFAULT 0 NOT NULL;
-
-ALTER TABLE ck_playeroptions2 ADD tips int(11) DEFAULT 1 NOT NULL;
---
+++ scripts/upgrade_scripts/upgrade_tips.sql
@@ -0,0 +1,1 @@
+ALTER TABLE ck_playeroptions2 ADD tips int(11) DEFAULT 1 NOT NULL;
